af93b796b78007f3a506eab1cb195f5e3bb3e4c9
Siva <svala@uic.edu>
2024-04-11 01:36:44 -0500

    Initialization and Register Page

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+const app = express();
+require("dotenv").config();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("MongoDB successfully connected"))
+    .catch(err => console.error("MongoDB connection error: ", err));
+
+
+const server = app.listen(process.env.PORT, () => {
+    console.log(`Server started on port ${process.env.PORT}`);
+});
